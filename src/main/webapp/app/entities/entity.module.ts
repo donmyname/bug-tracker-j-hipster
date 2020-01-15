7a89644b65731476109f7bd033b862451d7e680d
Bucky John <johns@jhk3po.com>
2020-01-15 16:25:46 +0200

    Adding a category Entity with jhipster generator - full stack

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'category',
+        loadChildren: () => import('./category/category.module').then(m => m.BugTrackerJHipsterCategoryModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
