e8103f09045f57ba7f08c08c83fba0e2855246fb
Bucky John <johns@jhk3po.com>
2020-01-18 17:40:56 +0200

    Adding generated Entity example from Online JDL Stodio on site with jhipster generator - full stack

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: () => import('./region/region.module').then(m => m.BugTrackerJHipsterRegionModule)
+      },
+      {
+        path: 'country',
+        loadChildren: () => import('./country/country.module').then(m => m.BugTrackerJHipsterCountryModule)
+      },
+      {
+        path: 'location',
+        loadChildren: () => import('./location/location.module').then(m => m.BugTrackerJHipsterLocationModule)
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('./department/department.module').then(m => m.BugTrackerJHipsterDepartmentModule)
+      },
+      {
+        path: 'task',
+        loadChildren: () => import('./task/task.module').then(m => m.BugTrackerJHipsterTaskModule)
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then(m => m.BugTrackerJHipsterEmployeeModule)
+      },
+      {
+        path: 'job',
+        loadChildren: () => import('./job/job.module').then(m => m.BugTrackerJHipsterJobModule)
+      },
+      {
+        path: 'job-history',
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.BugTrackerJHipsterJobHistoryModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
